4b537265e3c4ce87790e425bf4fabb4364139212
ydideh810 <yd.baller810@gmail.com>
2024-06-20 00:28:34 -0400

    Update footer.tsx

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
     return (
         <footer className='mt-24 flex h-32 flex-col items-center justify-between bg-gradient-to-b from-transparent to-primary/20'>
             <div className='p-5 text-center'>
-                Have any questions, reach out on  socials!{' '}
+                Niddam Industries. All rights reserved.{' '}
                 <span className='whitespace-nowrap '>
                     <a
                         href='mailto:ferjani.nader@hotmail.fr'
