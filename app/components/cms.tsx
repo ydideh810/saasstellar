7c6d1f0482a8ca64d3dcb8a037a0708062a98f66
ydideh810 <yd.baller810@gmail.com>
2024-06-20 13:14:09 -0400

    Update cms.tsx

--- app/components/cms.tsx
+++ app/components/cms.tsx
@@ -1,11 +1,59 @@
 import React from 'react'
-
+import Drupal from '~/components/icons/drupal'
+import Framer from '~/components/icons/framer'
+import Joomla from '~/components/icons/joomla'
+import Notion from '~/components/icons/notion'
+import Sanity from '~/components/icons/sanity'
+import Storyblok from '~/components/icons/storyblok'
+import Strapi from '~/components/icons/strapi'
+import Typedream from '~/components/icons/typedream'
+import Wordpress from '~/components/icons/wordpress'
 import {
     Tooltip,
     TooltipContent,
     TooltipProvider,
     TooltipTrigger,
 } from '~/components/ui/tooltip'
+
+const Cms = () => {
+    const cms = [
+        {
+            component: <Drupal height={42} width={42} />,
+            name: 'Drupal',
+        },
+        {
+            component: <Framer height={42} width={42} />,
+            name: 'Framer',
+        },
+        {
+            component: <Joomla height={42} width={42} />,
+            name: 'Joomla',
+        },
+        {
+            component: <Notion height={42} width={42} />,
+            name: 'Notion',
+        },
+        {
+            component: <Sanity height={42} width={42} />,
+            name: 'Sanity',
+        },
+        {
+            component: <Storyblok height={42} width={42} />,
+            name: 'Storyblok',
+        },
+        {
+            component: <Strapi height={42} width={42} />,
+            name: 'Strapi',
+        },
+        {
+            component: <Typedream height={42} width={42} />,
+            name: 'Typedream',
+        },
+        {
+            component: <Wordpress height={42} width={42} />,
+            name: 'Wordpress',
+        },
+    ]
 
 
 
